37733f54c4c0d55dd4e413cafdc8a666d4d841a4
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-12-18 13:14:31 -0800

    Solved day 3. Still need to clean up code

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -1,63 +1,63 @@
-use aoc::parse_regex::parse_lines;
-use regex::Regex;
+use itertools::Itertools;
+use std::collections::HashSet;
 
 static TEST_INPUT: &str = "
-A Y
-B X
-C Z
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
 ";
 
 fn main() {
-    let input = parse_input(include_str!("../../puzzle_inputs/day_02.txt"));
+    // Parse the input
+    let input: Vec<&str> = include_str!("../../puzzle_inputs/day_03.txt")
+        // let input: Vec<&str> = TEST_INPUT
+        .trim()
+        .lines()
+        .collect();
+
+    // Solve puzzle a
+    let answer: u32 = input
+        .iter()
+        .map(|line| {
+            let both_sacks: Vec<char> = line.chars().collect();
+            let n_items = both_sacks.len();
+            let sack_1: HashSet<&char> = both_sacks[0..(n_items / 2)].iter().collect();
+            let sack_2: HashSet<&char> = both_sacks[(n_items / 2)..n_items].iter().collect();
+            let common_item = **sack_1.intersection(&sack_2).next().unwrap();
+            priority(common_item)
+        })
+        .sum();
+    println!("answer a: {}", answer);
+
+    // Solve puzzle b
+    let answer: u32 = input
+        .iter()
+        .map(|line| line.chars().collect::<HashSet<char>>())
+        .tuples()
+        .map(|(sack_1, sack_2, sack_3)| {
+            let sack_12: HashSet<char> = sack_1.intersection(&sack_2).copied().collect();
+            let item = sack_12.intersection(&sack_3).next().unwrap();
+            priority(*item)
+        })
+        .sum();
+    println!("answer: {}", answer);
+
+    // let input = parse_input(include_str!("../../puzzle_inputs/day_02.txt"));
     // let input = parse_input(TEST_INPUT);
-
-    println!(
-        "day 2 a: {}",
-        input.iter().map(compute_score_a).sum::<u64>()
-    );
-
-    println!(
-        "day 2 b: {}",
-        input.iter().map(compute_score_b).sum::<u64>()
-    );
 }
 
-fn compute_score_a(round: &(char, char)) -> u64 {
-    let winner_score = match round {
-        &('A', 'Y') | &('B', 'Z') | &('C', 'X') => 6,
-        &('A', 'X') | &('B', 'Y') | &('C', 'Z') => 3,
-        _ => 0,
-    };
-
-    let choice_score = match round.1 {
-        'X' => 1,
-        'Y' => 2,
-        'Z' => 3,
-        c => unimplemented!("Unexpected play: {}", c),
-    };
-
-    winner_score + choice_score
+fn priority(item: char) -> u32 {
+    match item {
+        'a'..='z' => item as u32 - 'a' as u32 + 1,
+        'A'..='Z' => item as u32 - 'A' as u32 + 27,
+        item => unreachable!("Unexpected item: {}", item),
+    }
 }
 
-fn compute_score_b(round: &(char, char)) -> u64 {
-    let winner_score = match round.1 {
-        'X' => 0,
-        'Y' => 3,
-        'Z' => 6,
-        c => unimplemented!("Unexpected play: {}", c),
-    };
-
-    let choice_score = match round {
-        &('A', 'Y') | &('B', 'X') | &('C', 'Z') => 1,
-        &('B', 'Y') | &('C', 'X') | &('A', 'Z') => 2,
-        &('C', 'Y') | &('A', 'X') | &('B', 'Z') => 3,
-        &(c1, c2) => unimplemented!("Unexpected play: {} {}", c1, c2),
-    };
-
-    winner_score + choice_score
-}
-
-fn parse_input(input: &str) -> Vec<(char, char)> {
-    let re = Regex::new("(A|B|C) (X|Y|Z)").unwrap();
-    parse_lines(re, input).collect()
-}
+// fn parse_input(input: &str) -> Vec<(char, char)> {
+//     let re = Regex::new("(A|B|C) (X|Y|Z)").unwrap();
+//     parse_lines(re, input).collect()
+// }
