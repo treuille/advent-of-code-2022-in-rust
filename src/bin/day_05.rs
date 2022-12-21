0b8480fb3c1f2e1aa8275bb26974bc27a1a34262
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-12-21 04:48:03 -0800

    Solved day 5

--- src/bin/day_05.rs
+++ src/bin/day_05.rs
@@ -2,57 +2,49 @@ use aoc::parse_regex::parse_lines;
 use regex::Regex;
 use std::iter;
 
-static TEST_INPUT: &str = "
-    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2
-";
+type Stack = Vec<char>;
+type Instruction = (usize, usize, usize);
 
 fn main() {
+    // Split the input into two pieces
     let input = include_str!("../../puzzle_inputs/day_05.txt");
-    // let input = TEST_INPUT;
     let (stack_lines, instructions) = input.split_once("\n\n").unwrap();
+
+    // Parse the stacks
     let mut stack_lines = stack_lines.lines().rev();
     let n_stacks = stack_lines.next().unwrap().split("   ").count();
-    println!("n_stacks: {:?}", n_stacks);
-    let mut stacks: Vec<Vec<char>> = iter::repeat_with(Vec::new).take(n_stacks).collect();
-
-    println!("stacks: {:?}", stacks);
-
+    let mut stacks: Vec<Stack> = iter::repeat_with(Vec::new).take(n_stacks).collect();
     for line in stack_lines {
-        println!("- \"{}\"", line);
-        for (c, stack) in line.chars().skip(1).step_by(4).zip(&mut stacks) {
-            if c != ' ' {
-                stack.push(c);
+        for (item, stack) in line.chars().skip(1).step_by(4).zip(&mut stacks) {
+            if item != ' ' {
+                stack.push(item);
             }
         }
     }
-    println!("That's all! {:?}", stacks);
 
-    println!("instructions:\n{}", instructions);
+    // Parse the instructions
     let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
-    let instructions: Vec<(usize, usize, usize)> = parse_lines(re, instructions).collect();
+    let instructions: Vec<Instruction> = parse_lines(re, instructions).collect();
 
-    println!("day 5a: {}", solve_a(&stacks, &instructions));
+    // Print the solutions
+    println!("day 5a: {}", sim_crane(&stacks, &instructions, false));
+    println!("day 5b: {}", sim_crane(&stacks, &instructions, true));
 }
 
-fn solve_a(stacks: &[Vec<char>], instructions: &Vec<(usize, usize, usize)>) -> String {
+fn sim_crane(stacks: &[Stack], instructions: &[Instruction], cm_9001: bool) -> String {
+    // Clone the stack so we can mutate them.
     let mut stacks = Vec::from(stacks);
+
+    // Run the simulation.
     for (n_items, from_stack, to_stack) in instructions {
-        println!("{} items : {} -> {}", n_items, from_stack, to_stack);
-        for _ in 0..*n_items {
-            let item = stacks[from_stack - 1].pop().unwrap();
-            stacks[to_stack - 1].push(item);
-            println!("Now: {:?}", stacks);
+        let split_at = stacks[from_stack - 1].len() - n_items;
+        let mut items_to_xfer = stacks[from_stack - 1].split_off(split_at);
+        if !cm_9001 {
+            items_to_xfer.reverse();
         }
+        stacks[to_stack - 1].extend(items_to_xfer);
     }
 
-    // The final solution
+    // Return the top items from each stack as a string.
     stacks.iter().map(|stack| stack.last().unwrap()).collect()
 }
