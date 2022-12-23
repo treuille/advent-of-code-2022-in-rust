ba1e8d6736136d6669cdeb852ff11cefb125c105
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-12-23 08:37:50 -0800

    Solved 10b. Need to clean up code

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -1,20 +1,134 @@
 // use aoc::parse_regex::parse_lines;
 // // use itertools::Itertools;
 // use regex::Regex;
-// // use std::iter;
+use ndarray::{s, Array2};
+use std::iter;
 
 fn main() {
     // Parse the input.
     // let input = TEST_INPUT_1;
     // let input = TEST_INPUT_2;
     let input = include_str!("../../puzzle_inputs/day_10.txt");
+    let register_vals = parse_input(input);
 
-    // let re = Regex::new(r"(addx|noop) (\-?\d+)").unwrap();
-    // let input = parse_lines(re, input).collect();
+    println!("10a: {} (16020)", solve_a(&register_vals));
+    println!("10b: (ECZUZALR)");
+    solve_b(&register_vals);
 
-    // 20th, 60th, 100th, 140th, 180th, and 220th
-    // 19, 59, 99, 139, 179, 219
-    let answer: i32 = input
+    // let (w, h) = (40, 6);
+    // let compute_crt = |offset: i32, register_offset: i32| {
+    //     register_vals
+    //         .iter()
+    //         .zip(offset..)
+    //         .fold(
+    //             Array1::<bool>::default(w * h),
+    //             |mut crt, (register, cycle)| {
+    //                 let x = cycle % (w as i32);
+    //                 if (register + register_offset - x).abs() <= 1 {
+    //                     crt[[(cycle as usize) % (w * h)]] = true;
+    //                     // (() as usize
+    //                 }
+    //                 crt
+    //             },
+    //         )
+    //         .into_shape((w, h))
+    //         .unwrap()
+    // };
+
+    // for offset in -3..=3 {
+    //     for register_offset in -3..=3 {
+    //         println!("\noffset: {} register_offset: {}", offset, register_offset);
+    //         print_crt(compute_crt(offset, register_offset));
+    //     }
+    // }
+}
+
+fn solve_a(register_vals: &[i32]) -> i32 {
+    register_vals
+        .iter()
+        .zip(2..)
+        .filter_map(|(sum, i)| match i {
+            20 | 60 | 100 | 140 | 180 | 220 => Some(i * sum),
+            _ => None,
+        })
+        .sum()
+}
+
+fn solve_b(register_vals: &[i32]) {
+    let (w, h) = (40, 6);
+    let mut crt = Array2::<bool>::default((w, h));
+    let register_vals = iter::once(&1).chain(register_vals.iter());
+    for (&sprite_pos, cycle) in register_vals.zip(1usize..) {
+        // Figure out the pixel position
+        let pixel_x = (cycle - 1) % w;
+        let pixel_y = ((cycle - 1) / w) % h;
+
+        // Get the current row
+        let mut row = crt.slice_mut(s![.., pixel_y]);
+        if ((pixel_x as i32) - sprite_pos).abs() <= 1 {
+            row[pixel_x] = true;
+        }
+
+        // Print the current state.
+        println!("Cycle:{:>12}", cycle);
+        println!("Pixel:           ({},{})", pixel_x, pixel_y);
+        println!("Sprite Position: {:}", sprite_pos);
+        // println!("Row dim: {:?}", row.dim());
+
+        // Print the sprite
+        print!("Sprite:          ");
+        for x in 0..(w as i32) {
+            if (x - sprite_pos).abs() <= 1 {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+
+        // Print the current row
+        print!("Current row:     ");
+        for x in 0..=pixel_x {
+            if row[x] {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+
+        // Print the crt
+        println!();
+        print_crt(&crt);
+
+        // Print spacer
+        println!();
+
+        // // Break early
+        // if cycle == 22 {
+        //     break;
+        // }
+    }
+}
+
+fn print_crt(crt: &Array2<bool>) {
+    let (w, h) = crt.dim();
+    for j in 0..h {
+        for i in 0..w {
+            print!(
+                "{}",
+                match crt.get((i, j)).unwrap() {
+                    false => ".",
+                    true => "#",
+                }
+            );
+        }
+        println!();
+    }
+}
+
+fn parse_input(input: &str) -> Vec<i32> {
+    input
         .trim()
         .lines()
         .flat_map(|line| {
@@ -29,23 +143,7 @@ fn main() {
             *sum += x;
             Some(*sum)
         })
-        // .enumerate()
-        .zip(2..)
-        .filter_map(|(sum, i)| match i {
-            // 18 | 58 | 98 | 138 | 178 | 218 => Some((i, sum)),
-            // 19 | 59 | 99 | 139 | 179 | 219 => Some((i, sum)),
-            20 | 60 | 100 | 140 | 180 | 220 => Some(i * sum),
-            // 21 | 61 | 101 | 141 | 181 | 221 => Some((i, sum)),
-            // 22 | 62 | 102 | 142 | 182 | 222 => Some((i, sum)),
-            _ => None,
-        })
-        // .for_each(|(i, x): (usize, i32)| println!("{} -> {:?}", i, x));
-        .sum();
-    println!("answer: {}", answer);
-
-    // wu for i in 0..10 {
-    //     println!("{}: Hello, world!", i);
-    // }
+        .collect()
 }
 
 const TEST_INPUT_1: &str = "
