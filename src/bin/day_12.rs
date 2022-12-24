0f21fce0d9b6e11df587d49a63d6c2e4c12be7a0
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-12-23 19:28:33 -0800

    Solved day 12

--- src/bin/day_12.rs
+++ src/bin/day_12.rs
@@ -1,71 +1,72 @@
-use aoc::grid::parse_char_grid;
+use aoc::grid::{neighbors, parse_char_grid};
 use core::cmp::Reverse;
 use ndarray::Array2;
 use std::collections::BinaryHeap;
 use std::convert::identity;
 use std::iter;
 
-const TEST_INPUT: &str = "
-Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi
-";
-
 fn main() {
-    println!("day 12");
-
-    // let input = include_str!("../../puzzle_inputs/day_08.txt");
-    let input = TEST_INPUT;
+    let input = include_str!("../../puzzle_inputs/day_12.txt");
     let mut height_map = parse_char_grid(input, identity);
-
-    println!("height_map:\n{:?}", height_map);
 
     // Figure out the starting and ending points
     let (mut start, mut end) = (None, None);
-    for (pos, val) in height_map.indexed_iter_mut() {
-        if *val == 'S' {
+    for (pos, height) in height_map.indexed_iter_mut() {
+        if *height == 'S' {
             start = Some(pos);
-            *val = 'a';
-        } else if *val == 'E' {
+            *height = 'a';
+        } else if *height == 'E' {
             end = Some(pos);
-            *val = 'z';
+            *height = 'z';
         }
         if start.is_some() && end.is_some() {
             break;
         }
     }
+
+    // Solve part a
     let (start, end) = (start.unwrap(), end.unwrap());
-    println!("height_map:\n{:?}", height_map);
-    println!("start: {:?}, end: {:?}", start, end);
+    println!("day 12a: {} (447)", solve(&height_map, start, end).unwrap());
+
+    // Solve part b
+    println!(
+        "day 12b: {} (446)",
+        height_map
+            .indexed_iter()
+            .filter_map(|(pos, height)| (*height == 'a').then_some(pos))
+            .filter_map(|start| solve(&height_map, start, end))
+            .min()
+            .unwrap()
+    );
+}
+
+fn solve(height_map: &Array2<char>, start: (usize, usize), end: (usize, usize)) -> Option<usize> {
+    let can_step = |c1: char, c2: char| -> bool {
+        (c2 as u32)
+            .checked_sub(c1 as u32)
+            .map(|diff| diff <= 1)
+            .unwrap_or(true)
+    };
 
     // Find the shortest path
-    let steps = Array2::<Option<usize>>::default(height_map.dim());
+    let dim = height_map.dim();
+    let mut steps = Array2::<Option<usize>>::default(dim);
     let mut heap: BinaryHeap<_> = iter::once(Reverse((0, start))).collect();
     while let Some(Reverse((step, pos))) = heap.pop() {
         if pos == end {
-            panic!("Found the end in {} steps", step);
-        }
-
-        if steps[pos] == Some(step) {
+            return Some(step);
+        } else if steps[pos] == Some(step) {
             continue;
         }
 
-        panic!("Somethign");
-        // steps[pos] = Some(
-        // for &dir in &[(0, 1), (0, -1), (1, 0), (-1, 0)] {
-        //     let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
-        //     if height_map[new_pos] != '#' && steps[new_pos] != Some(step + 1) {
-        //         heap.push(Reverse((step + 1, new_pos)));
-        //     }
-        // }
+        steps[pos] = Some(step);
+        for neighbor in neighbors(pos, dim) {
+            if steps[neighbor].is_some() {
+                continue;
+            } else if can_step(height_map[pos], height_map[neighbor]) {
+                heap.push(Reverse((step + 1, neighbor)));
+            }
+        }
     }
-    println!("heap: {:?}", heap);
-    // while let Some(item) = heap.pop() {
-    //     println!("item: {:?}", item);
-    // }
-    // let input = include_str!("../../puzzle_inputs/day_11.txt");
-    // o
-    // let monkeys: Vec<Monkey> = input.trim().split("\n\n").map(Monkey::from_str).collect();
+    None
 }
